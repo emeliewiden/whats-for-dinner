c6f408b8e1c12194d4013ce5f3e7fe03cc0ae513
Emelie Widén <emelie.widen@icloud.com>
2024-05-09 23:02:58 +0200

    Bakgrundsbild, container för innehåll, färger = ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-container">
+          <h1>Whats for dinner?</h1>
+          <h2 id="answer"> </h2>
+          <button id="button">Surprise me!</button>
+          </div>
+        </header>
+      </div>
+    </main>
   );
 }
 
